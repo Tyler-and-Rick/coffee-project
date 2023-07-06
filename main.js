6357d74c3240794e55c923b19ab9ff92022321be
Rick Morales <rick.morales7693@gmail.com>
2023-07-06 16:25:38 -0500

    added icons and giphs

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ const searchInputElement = document.querySelector("#coffee-search");
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light', },
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'lightsaber', },
+    {id: 2, name: 'Half City', roast: 'lightsaber'},
+    {id: 3, name: 'Cinnamon', roast: 'lightsaber'},
+    {id: 4, name: 'City', roast: 'mediumfalcon'},
+    {id: 5, name: 'American', roast: 'mediumfalcon'},
+    {id: 6, name: 'Breakfast', roast: 'mediumfalcon'},
+    {id: 7, name: 'High', roast: 'darkvader'},
+    {id: 8, name: 'Continental', roast: 'darkvader'},
+    {id: 9, name: 'New Orleans', roast: 'darkvader'},
+    {id: 10, name: 'European', roast: 'darkvader'},
+    {id: 11, name: 'Espresso', roast: 'darkvader'},
+    {id: 12, name: 'Viennese', roast: 'darkvader'},
+    {id: 13, name: 'Italian', roast: 'darkvader'},
+    {id: 14, name: 'French', roast: 'darkvader'},
 ];
 
 (() => {
