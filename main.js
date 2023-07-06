d40df454c98737fba2974776d27d795239db2be1
Rick Morales <rick.morales7693@gmail.com>
2023-07-06 14:08:27 -0500

    added new background image

--- main.js
+++ main.js
@@ -2,6 +2,31 @@
 
 const searchInputElement = document.querySelector("#coffee-search");
 
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+let coffees = [
+    {id: 1, name: 'Light City', roast: 'light', },
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+(() => {
+    let localStorageItem = localStorage.getItem("storedCoffee");
+    if(localStorageItem){
+        coffees.push(JSON.parse(localStorageItem));
+    }
+})()
+
 function renderCoffee(coffee) {
     // let coffeeImage = coffee.image ? coffee.image : "http://via.placeholder.com/150";
     let html = `
@@ -57,7 +82,6 @@ function addCoffees(e) {
     const userDropdownAddition = dropdownAddition.value;
     const userSearchAddition = document.querySelector('#coffee-addition').value.toLowerCase();
 
-    console.log(userDropdownAddition);
 
     let coffee = {
         id: identifier,
@@ -66,37 +90,15 @@ function addCoffees(e) {
     }
 
     identifier++;
-    console.log(coffee);
-
     coffees.push(coffee);
+    let coffeeJSON = JSON.stringify(coffee);
+    localStorage.setItem('storedCoffee', coffeeJSON);
     tbody.innerHTML = renderCoffees(coffees);
 
+    // let restoredCoffee = localStorage.getItem(storedCoffee);
+    // let coffeeObject = JSON.parse(restoredCoffee);
+    // document.getElementById(coffeeObject).innerHTML = coffeeObject.name;
 }
-
-
-
-
-
-
-
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-let coffees = [
-    {id: 1, name: 'Light City', roast: 'light', },
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
 
 let tbody = document.querySelector('#coffees');
 let dropdown = document.querySelector('#roast-selection');
